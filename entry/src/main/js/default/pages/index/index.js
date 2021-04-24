e40dda525f19940c93802cc230593c70246b118e
molifei <574579565@qq.com>
2021-04-24 17:52:36 +0800

    feat：新增组件

--- entry/src/main/js/default/pages/index/index.js
+++ entry/src/main/js/default/pages/index/index.js
@@ -14,6 +14,49 @@ export default {
              date: '2021-12-31 20:00:00',
            }
     ],
+    toggle_list: [
+        {
+          "id": "1001",
+          "name": "Living room",
+          "checked": true
+        },
+        {
+          "id": "1002",
+          "name": "Bedroom",
+          "checked": false
+        },
+        {
+          "id": "1003",
+          "name": "Second bedroom",
+          "checked": false
+        },
+        {
+          "id": "1004",
+          "name": "Kitchen",
+          "checked": false
+        },
+        {
+          "id": "1005",
+          "name": "Study",
+          "checked": false
+        },
+        {
+          "id": "1006",
+          "name": "Garden",
+          "checked": false
+        },
+        {
+          "id": "1007",
+          "name": "Bathroom",
+          "checked": false
+        },
+        {
+          "id": "1008",
+          "name": "Balcony",
+          "checked": false
+        },
+    ],
+    idx: '',
   },
   onInit() {
     this.title = this.title + '123'
@@ -24,9 +67,11 @@ export default {
     fetch.fetch({
       url: 'http://127.0.0.1:4523/mock/379821/table/list',
       success: function (response) {
+        console.info(response)
         console.info("fetch success");
       },
-      fail: function () {
+      fail: function (e) {
+        console.info(e)
         console.info("fetch fail");
       }
     });
@@ -36,7 +81,21 @@ export default {
   },
   goToOther() {
     router.push({
-      uri:"pages/welcome/welcome"
+      uri: "pages/welcome/welcome"
     })
+  },
+  allClick(arg) {
+    this.idx = arg
+  },
+  allChange(e) {
+    if (e.checked === true) {
+      for (var i = 0; i < this.toggle_list.length; i++) {
+        if (this.toggle_list[i].id === this.idx) {
+          this.toggle_list[i].checked = true
+        } else {
+          this.toggle_list[i].checked = false
+        }
+      }
+    }
   }
 }
